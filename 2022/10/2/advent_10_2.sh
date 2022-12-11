d6eabde593fefa685c7a0ffec6c87545911a0b8d
CodingMarkus <CodingMarkus.Commit.2022@quantentunnel.de>
2022-12-11 23:43:09 +0100

    Add solution for part 2 of day 10

---
+++ 2022/10/2/advent_10_2.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Run as:
+# cat advent_10_2_sample.txt | sh advent_10_2.sh
+# cat advent_10_2_input.txt  | sh advent_10_2.sh
+
+set -e
+
+x=2
+cycles=1
+while read -r instruction value
+do
+	case $instruction in
+		noop) delay=1;;
+		addx) delay=2;;
+		*) echo "Error: Bad instructions"; exit 1;;
+	esac
+
+	while [ $delay -gt 0 ]
+	do
+		if [ $(( cycles > x ? cycles - x : x - cycles )) -lt 2 ]
+		then
+			printf '#'
+		else
+			printf '.'
+		fi
+		if [ $(( cycles % 40 )) -eq 0 ]
+		then
+			printf '\n'
+			cycles=1;
+		else
+			cycles=$(( cycles + 1 ))
+		fi
+		delay=$(( delay - 1 ))
+	done
+
+	if [ "$instruction" = "addx" ]
+	then
+		x=$(( x + value ))
+	fi
+done
+
+echo
